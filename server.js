be03e10cf778b42b53a6dcfac77e7b9e85b3f76d
Siddharth Sharma <hearsid@gmail.com>
2015-11-18 20:58:03 +0530

    node server added for heroku

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 /**
  * Created by sid on 18/11/15.
  */
-var express = require('express') ;
+var express = require('express');
+var app = express();
+var http = require('http');
+var httpServer = http.Server(app);
 
-var app = express() ;
+app.use(express.static(__dirname+'/app'));
+
+app.get('/', function(req, res){
+    res.sendfile(__dirname + '/index.html');
+});
+app.listen(3000);
